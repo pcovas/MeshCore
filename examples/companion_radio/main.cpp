4b1cea07f00d3812876065685951a8b9338c4ffd
Daniel <laser297@yahoo.com>
2026-03-09 18:11:55 +0000

    migração concluida para 14.1 com ble e wifi

--- examples/companion_radio/main.cpp
+++ examples/companion_radio/main.cpp
@@ -1,6 +1,25 @@
-#include <Arduino.h>   // needed for PlatformIO
+#include <Arduino.h>
 #include <Mesh.h>
 #include "MyMesh.h"
+
+#include <WiFi.h>
+
+// Interfaces
+#include "helpers/esp32/SerialBLEInterface.h"
+#include "helpers/esp32/SerialWifiInterface.h"
+#include "helpers/esp32/MultiInterface.h"
+
+#define DEBUG_SERIAL Serial
+
+String g_wifi_ip = "";
+
+SerialBLEInterface ble_if;
+SerialWifiInterface wifi_if;
+MultiInterface serial_interface;
+
+#ifndef TCP_PORT
+#define TCP_PORT 5000
+#endif
 
 // Believe it or not, this std C function is busted on some platforms!
 static uint32_t _atoi(const char* sp) {
@@ -12,80 +31,35 @@ static uint32_t _atoi(const char* sp) {
   return n;
 }
 
+/* ---------------- FILESYSTEM ---------------- */
 #if defined(NRF52_PLATFORM) || defined(STM32_PLATFORM)
   #include <InternalFileSystem.h>
   #if defined(QSPIFLASH)
     #include <CustomLFS_QSPIFlash.h>
     DataStore store(InternalFS, QSPIFlash, rtc_clock);
   #else
-  #if defined(EXTRAFS)
-    #include <CustomLFS.h>
-    CustomLFS ExtraFS(0xD4000, 0x19000, 128);
-    DataStore store(InternalFS, ExtraFS, rtc_clock);
-  #else
-    DataStore store(InternalFS, rtc_clock);
+    #if defined(EXTRAFS)
+      #include <CustomLFS.h>
+      CustomLFS ExtraFS(0xD4000, 0x19000, 128);
+      DataStore store(InternalFS, ExtraFS, rtc_clock);
+    #else
+      DataStore store(InternalFS, rtc_clock);
+    #endif
   #endif
-  #endif
+
 #elif defined(RP2040_PLATFORM)
   #include <LittleFS.h>
   DataStore store(LittleFS, rtc_clock);
+
 #elif defined(ESP32)
   #include <SPIFFS.h>
   DataStore store(SPIFFS, rtc_clock);
-#endif
 
-#ifdef ESP32
-  #ifdef WIFI_SSID
-    #include <helpers/esp32/SerialWifiInterface.h>
-    SerialWifiInterface serial_interface;
-    #ifndef TCP_PORT
-      #define TCP_PORT 5000
-    #endif
-  #elif defined(BLE_PIN_CODE)
-    #include <helpers/esp32/SerialBLEInterface.h>
-    SerialBLEInterface serial_interface;
-  #elif defined(SERIAL_RX)
-    #include <helpers/ArduinoSerialInterface.h>
-    ArduinoSerialInterface serial_interface;
-    HardwareSerial companion_serial(1);
-  #else
-    #include <helpers/ArduinoSerialInterface.h>
-    ArduinoSerialInterface serial_interface;
-  #endif
-#elif defined(RP2040_PLATFORM)
-  //#ifdef WIFI_SSID
-  //  #include <helpers/rp2040/SerialWifiInterface.h>
-  //  SerialWifiInterface serial_interface;
-  //  #ifndef TCP_PORT
-  //    #define TCP_PORT 5000
-  //  #endif
-  // #elif defined(BLE_PIN_CODE)
-  //   #include <helpers/rp2040/SerialBLEInterface.h>
-  //   SerialBLEInterface serial_interface;
-  #if defined(SERIAL_RX)
-    #include <helpers/ArduinoSerialInterface.h>
-    ArduinoSerialInterface serial_interface;
-    HardwareSerial companion_serial(1);
-  #else
-    #include <helpers/ArduinoSerialInterface.h>
-    ArduinoSerialInterface serial_interface;
-  #endif
-#elif defined(NRF52_PLATFORM)
-  #ifdef BLE_PIN_CODE
-    #include <helpers/nrf52/SerialBLEInterface.h>
-    SerialBLEInterface serial_interface;
-  #else
-    #include <helpers/ArduinoSerialInterface.h>
-    ArduinoSerialInterface serial_interface;
-  #endif
-#elif defined(STM32_PLATFORM)
-  #include <helpers/ArduinoSerialInterface.h>
-  ArduinoSerialInterface serial_interface;
 #else
-  #error "need to define a serial interface"
+  #error "Unsupported platform"
 #endif
 
-/* GLOBAL OBJECTS */
+/* ---------------- GLOBAL OBJECTS ---------------- */
 #ifdef DISPLAY_CLASS
   #include "UITask.h"
   UITask ui_task(&board, &serial_interface);
@@ -93,136 +67,150 @@ static uint32_t _atoi(const char* sp) {
 
 StdRNG fast_rng;
 SimpleMeshTables tables;
-MyMesh the_mesh(radio_driver, fast_rng, rtc_clock, tables, store
-   #ifdef DISPLAY_CLASS
-      , &ui_task
-   #endif
+
+MyMesh the_mesh(
+    radio_driver,
+    fast_rng,
+    rtc_clock,
+    tables,
+    store
+#ifdef DISPLAY_CLASS
+    , &ui_task
+#endif
 );
 
-/* END GLOBAL OBJECTS */
-
+/* ---------------- HALT ---------------- */
 void halt() {
-  while (1) ;
+  while (1);
 }
 
+/* ======================================================
+ *                        SETUP
+ * ====================================================== */
 void setup() {
   Serial.begin(115200);
+  delay(200);
+  Serial.println("USB OK");
+
+  DEBUG_SERIAL.begin(115200);
+  delay(200);
+  DEBUG_SERIAL.println("Debug ativo");
 
   board.begin();
 
-#ifdef DISPLAY_CLASS
+  /* DISPLAY INIT */
   DisplayDriver* disp = NULL;
+#ifdef DISPLAY_CLASS
   if (display.begin()) {
     disp = &display;
     disp->startFrame();
-  #ifdef ST7789
-    disp->setTextSize(2);
-  #endif
     disp->drawTextCentered(disp->width() / 2, 28, "Loading...");
     disp->endFrame();
   }
 #endif
 
-  if (!radio_init()) { halt(); }
-
+  /* RADIO INIT */
+  if (!radio_init()) halt();
   fast_rng.begin(radio_get_rng_seed());
 
-#if defined(NRF52_PLATFORM) || defined(STM32_PLATFORM)
-  InternalFS.begin();
-  #if defined(QSPIFLASH)
-    if (!QSPIFlash.begin()) {
-      // debug output might not be available at this point, might be too early. maybe should fall back to InternalFS here?
-      MESH_DEBUG_PRINTLN("CustomLFS_QSPIFlash: failed to initialize");
-    } else {
-      MESH_DEBUG_PRINTLN("CustomLFS_QSPIFlash: initialized successfully");
-    }
-  #else
-  #if defined(EXTRAFS)
-      ExtraFS.begin();
-  #endif
-  #endif
+  /* FILESYSTEM INIT */
+#if defined(ESP32)
+  if (!SPIFFS.begin(false)) {
+    Serial.println("[SPIFFS] Mount failed, formatting...");
+    SPIFFS.begin(true);
+  }
+#endif
   store.begin();
+
+  /* MESH INIT */
   the_mesh.begin(
-    #ifdef DISPLAY_CLASS
-        disp != NULL
-    #else
-        false
-    #endif
+#ifdef DISPLAY_CLASS
+      disp != NULL
+#else
+      false
+#endif
   );
 
+  /* ======================================================
+   *                BLE INIT
+   * ====================================================== */
 #ifdef BLE_PIN_CODE
-  serial_interface.begin(BLE_NAME_PREFIX, the_mesh.getNodePrefs()->node_name, the_mesh.getBLEPin());
-#else
-  serial_interface.begin(Serial);
+  {
+    char dev_name[48];
+sprintf(dev_name, "%s", BLE_NAME_PREFIX);
+
+ble_if.begin(dev_name, the_mesh.getNodePrefs()->node_name, the_mesh.getBLEPin());
+serial_interface.setBLE(&ble_if);
+
+  }
 #endif
-  the_mesh.startInterface(serial_interface);
-#elif defined(RP2040_PLATFORM)
-  LittleFS.begin();
-  store.begin();
-  the_mesh.begin(
-    #ifdef DISPLAY_CLASS
-        disp != NULL
-    #else
-        false
-    #endif
-  );
 
-  //#ifdef WIFI_SSID
-  //  WiFi.begin(WIFI_SSID, WIFI_PWD);
-  //  serial_interface.begin(TCP_PORT);
-  // #elif defined(BLE_PIN_CODE)
-  //   char dev_name[32+16];
-  //   sprintf(dev_name, "%s%s", BLE_NAME_PREFIX, the_mesh.getNodeName());
-  //   serial_interface.begin(dev_name, the_mesh.getBLEPin());
-  #if defined(SERIAL_RX)
-    companion_serial.setPins(SERIAL_RX, SERIAL_TX);
-    companion_serial.begin(115200);
-    serial_interface.begin(companion_serial);
-  #else
-    serial_interface.begin(Serial);
-  #endif
-    the_mesh.startInterface(serial_interface);
-#elif defined(ESP32)
-  SPIFFS.begin(true);
-  store.begin();
-  the_mesh.begin(
-    #ifdef DISPLAY_CLASS
-        disp != NULL
-    #else
-        false
-    #endif
-  );
-
+  /* ======================================================
+   *                WIFI INIT (TCP)
+   * ====================================================== */
 #ifdef WIFI_SSID
-  board.setInhibitSleep(true);   // prevent sleep when WiFi is active
+  WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PWD);
-  serial_interface.begin(TCP_PORT);
-#elif defined(BLE_PIN_CODE)
-  serial_interface.begin(BLE_NAME_PREFIX, the_mesh.getNodePrefs()->node_name, the_mesh.getBLEPin());
-#elif defined(SERIAL_RX)
-  companion_serial.setPins(SERIAL_RX, SERIAL_TX);
-  companion_serial.begin(115200);
-  serial_interface.begin(companion_serial);
-#else
-  serial_interface.begin(Serial);
-#endif
-  the_mesh.startInterface(serial_interface);
-#else
-  #error "need to define filesystem"
+
+  Serial.println("[WiFi] Connecting…");
+  unsigned long start = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - start < 15000) {
+    delay(200);
+    Serial.print(".");
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    for (int i = 0; i < 10 && WiFi.localIP().toString() == "0.0.0.0"; i++) {
+      delay(500);
+      Serial.println("[WiFi] Waiting for DHCP…");
+    }
+
+    if (WiFi.localIP().toString() != "0.0.0.0") {
+      g_wifi_ip = WiFi.localIP().toString();
+      Serial.print("[WiFi] IP: ");
+      Serial.println(g_wifi_ip);
+
+      wifi_if.begin(TCP_PORT);
+      serial_interface.setWiFi(&wifi_if);
+    } else {
+      Serial.println("[WiFi] DHCP failed");
+      g_wifi_ip = "";
+    }
+  } else {
+    Serial.println("[WiFi] Connection failed");
+    g_wifi_ip = "";
+  }
 #endif
 
+  /* ======================================================
+   *                SELECT INITIAL TRANSPORT
+   * ====================================================== */
+  if (serial_interface.ble != nullptr) {
+    serial_interface.setMode(MultiInterface::Mode::BLE);
+  } else if (serial_interface.wifi != nullptr) {
+    serial_interface.setMode(MultiInterface::Mode::WIFI);
+  }
+
+  the_mesh.startInterface(serial_interface);
+
+  /* SENSORS */
   sensors.begin();
 
 #ifdef DISPLAY_CLASS
-  ui_task.begin(disp, &sensors, the_mesh.getNodePrefs());  // still want to pass this in as dependency, as prefs might be moved
+  ui_task.begin(disp, &sensors, the_mesh.getNodePrefs());
 #endif
 }
 
+/* ======================================================
+ *                        LOOP
+ * ====================================================== */
 void loop() {
   the_mesh.loop();
   sensors.loop();
+
 #ifdef DISPLAY_CLASS
   ui_task.loop();
 #endif
+
   rtc_clock.tick();
 }
